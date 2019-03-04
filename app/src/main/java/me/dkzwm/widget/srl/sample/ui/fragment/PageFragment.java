f1377a8df1e6965a0076eb165bab50770ec3becc
dkzwm <412946760@qq.com>
2019-03-04 14:17:01 +0800

    Optimization

--- app/src/main/java/me/dkzwm/widget/srl/sample/ui/fragment/PageFragment.java
+++ app/src/main/java/me/dkzwm/widget/srl/sample/ui/fragment/PageFragment.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import me.dkzwm.widget.srl.sample.R;
