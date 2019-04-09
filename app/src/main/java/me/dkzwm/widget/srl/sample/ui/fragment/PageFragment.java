0f8c268e586e87ef0a91e308a13558b489d675e7
dkzwm <412946760@qq.com>
2019-04-09 17:26:01 +0800

    Optimization

--- app/src/main/java/me/dkzwm/widget/srl/sample/ui/fragment/PageFragment.java
+++ app/src/main/java/me/dkzwm/widget/srl/sample/ui/fragment/PageFragment.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
